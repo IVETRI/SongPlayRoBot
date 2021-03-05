1b3685ff1964c10e3e20955763f1755959735df2
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-03-05 10:14:24 -0800

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -5,9 +5,9 @@ from pytube import YouTube
 from pyrogram.types import InlineKeyboardMarkup
 from pyrogram.types import InlineKeyboardButton
 from youtubesearchpython import VideosSearch
-from Sherlock.Sherlock import ignore_blacklisted_users, get_arg
-from Sherlock import app, LOGGER
-from Sherlock.sql.chat_sql import add_chat_to_db
+from TamilBots.TamilBots import ignore_blacklisted_users, get_arg
+from TamilBots import app, LOGGER
+from TamilBots.sql.chat_sql import add_chat_to_db
 
 
 def yt_search(song):
