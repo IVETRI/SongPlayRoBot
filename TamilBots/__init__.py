6a7e5b2d038aefad427d4ebd23cc70c3c3e502c5
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-03-05 10:02:34 -0800

    Create __init__.py

---
+++ TamilBots/__init__.py
@@ -0,0 +1,11 @@
+import logging
+from pyrogram import Client
+from config import API_HASH, API_ID, BOT_TOKEN
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+LOGGER = logging.getLogger(__name__)
+
+app = Client("AlexaSongBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
