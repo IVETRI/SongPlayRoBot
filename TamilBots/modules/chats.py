582a4f988c5f6f8e8c3a0c62d17c297dc0604501
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-03-05 10:12:41 -0800

    Update chats.py

--- TamilBots/modules/chats.py
+++ TamilBots/modules/chats.py
@@ -1,8 +1,8 @@
 from config import OWNER_ID
 from pyrogram import filters
-from Sherlock import app
-from Sherlock.Sherlock import get_arg
-from Sherlock.sql.chat_sql import load_chats_list, remove_chat_from_db
+from TamilBots import app
+from TamilBots.Sherlock import get_arg
+from TamilBots.sql.chat_sql import load_chats_list, remove_chat_from_db
 from io import BytesIO
 
 
