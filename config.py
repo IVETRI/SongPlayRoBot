87b03df256a7c94d88fa1db13ecbdfa3f3cde274
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-03-05 09:56:43 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
