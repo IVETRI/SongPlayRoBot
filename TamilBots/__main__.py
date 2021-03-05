52cfbc2d9d7986f556f54b433016e3f1b1b93767
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-03-05 10:51:33 -0800

    Update __main__.py

--- TamilBots/__main__.py
+++ TamilBots/__main__.py
@@ -40,10 +40,10 @@ async def start(client, message):
                 ]
             ]
         )
-   else:
-       btn = None
-   await message.reply(start_text.format(name, user_id), reply_markup=btn)
-   add_chat_to_db(str(chat_id))
+    else:
+        btn = None
+    await message.reply(start_text.format(name, user_id), reply_markup=btn)
+    add_chat_to_db(str(chat_id))
 
 
 @app.on_message(filters.create(ignore_blacklisted_users) & filters.command("help"))
